8486e810111dde7d1eb5f91132400d6af60a24e1
Aradhya <soularadhya@gmail.com>
2026-04-10 23:51:49 +0530

    Add Arrays topic with basic practice questions

---
+++ Arrays/app.js
@@ -0,0 +1,19 @@
+// TOPIC: Arrays in JavaScript
+
+// Q1: PRINT ALL ELEMENTS OF AN ARRAY
+/*
+let arr = [10, 20, 30, 40, 50];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+*/
+
+// Q2: FIND SUM OF ALL ELEMENTS IN AN ARRAY
+/*
+let numbers = [5, 10, 15, 20];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log("Sum of elements is: " + sum);
+*/
