c2458d666c1d00f5380ba17a271f55a90a0a39f7
Aradhya <soularadhya@gmail.com>
2026-04-24 23:53:58 +0530

    Add prepend method and Q2: Prepend elements to Linked List

--- LinkedLists/app.js
+++ LinkedLists/app.js
@@ -26,6 +26,13 @@ class LinkedList {
         current.next = newNode;
     }
 
+    // Insert at the beginning
+    prepend(data) {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
     // Print the list
     printList() {
         let current = this.head;
@@ -46,4 +53,14 @@ list.append(20);
 list.append(30);
 console.log("Linked List:");
 list.printList();
+*/
+
+// Q2: PREPEND ELEMENTS TO A LINKED LIST
+/*
+const list2 = new LinkedList();
+list2.prepend(30);
+list2.prepend(20);
+list2.prepend(10);
+console.log("Linked List after prepending:");
+list2.printList();
 */
